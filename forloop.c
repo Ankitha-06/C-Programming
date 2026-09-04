d0f7c49400b422d97940bf994f9d0a9f44c8c1ea
P Ankitha Rani <pankitharani.sirwar@gmail.com>
2026-09-04 18:30:41 +0530

    Add files via upload

---
+++ forloop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    for(int i=0;i<=10;i++){
+        printf("Hello World\n");
+    }
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+     for (int i=0;i<=n;i++){
+        printf("%d\n",i);
+     }
+}

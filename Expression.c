27b28f24dae63cee01d5d422631a635bc45b990b
P Ankitha Rani <pankitharani.sirwar@gmail.com>
2026-09-08 17:36:49 +0530

    Add files via upload

---
+++ Expression.c
@@ -0,0 +1,7 @@
+//10+5*2 with (10+5)*2
+#include<stdio.h>
+int main(){
+    int a=10+5*2,b=(10+5)*2;
+    printf("%d %d",a,b);
+    return 0;
+}
---
+++ Function.c
@@ -0,0 +1,9 @@
+//Write a function without arguments and without a return type.
+#include<stdio.h>
+void display(){ 
+    printf("Hello World !\n ");
+}
+int main(){
+    display();
+    display();//function call
+}

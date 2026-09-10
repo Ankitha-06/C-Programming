b775d378b49458f8c84b1d1a2f78760505402fac
P Ankitha Rani <pankitharani.sirwar@gmail.com>
2026-09-10 14:48:52 +0530

    Add files via upload

---
+++ Add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=20;
+    int b=10;
+    int sum=a+b;
+    int c=10;
+    int nw_sum=a+c;
+    printf("%d\n",sum);
+    printf("%d",nw_sum);
+    return 0;
+
+}
